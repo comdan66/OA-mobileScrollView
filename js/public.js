3a019d2163b2867fa93dc9a1d8b5386ed4a849e1
OA Wu <comdan66@gmail.com>
2016-03-14 16:44:09 +0800

    Fix code

--- js/public.js
+++ js/public.js
@@ -18,5 +18,7 @@ $(function () {
   $('#fb').click (function () {
     window.open ('https://www.facebook.com/sharer/sharer.php?u=' + window.location.href, '分享', 'scrollbars=yes,resizable=yes,toolbar=no,location=yes,width=550,height=420,top=100,left=' + (window.screen ? Math.round(screen.width / 2 - 275) : 100));
   });
-  $('.scroll').OAmobileScrollView ();
+  $('.scroll').OAmobileScrollView ({
+    trigger_length: 30
+  });
 });
