dc2a2c5a59a0ef5708578aea7fd8d7b1510261d0
OA Wu <comdan66@gmail.com>
2016-03-14 12:57:10 +0800

    Add index.html js feature.

--- js/public.js
+++ js/public.js
@@ -4,5 +4,11 @@
  */
 
 $(function () {
-
+  $('.icon-more').click (function () {
+    $(this).toggleClass ('s');
+  });
+  $('#fb').click (function () {
+    window.open ('https://www.facebook.com/sharer/sharer.php?u=' + window.location.href, '分享', 'scrollbars=yes,resizable=yes,toolbar=no,location=yes,width=550,height=420,top=100,left=' + (window.screen ? Math.round(screen.width / 2 - 275) : 100));
+  });
+  $('.scroll').OAmobileScrollView ();
 });
